0c863e926b14063262dfcc1335ecea53e52a186e
Pan-Grayza <zagraystepan@gmail.com>
2023-09-08 16:46:41 +0100

    Readded 'baseUrl': '.' to tsconfig

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -44,133 +44,135 @@ const Sidebar: React.FC<Props> = ({ className }) => {
         'w-0 animate-sidebarShrinking': !sidebar,
       })}
     >
-      <div
-        className={clsx(
-          'absolute -left-60 top-0 z-50 flex h-full w-60 flex-col items-center gap-1 overflow-y-auto p-2 transition',
-          { 'translate-x-60': sidebar, 'translate-x-0': !sidebar },
-          className
-        )}
-      >
-        <MonthCell size="xs" sidebar monthDate={currentDate} />
-        <div className="relative flex h-fit w-full flex-col">
-          {calendars?.map((calendar, index) => {
-            return (
+      {selectedCalendar && (
+        <div
+          className={clsx(
+            'absolute -left-60 top-0 z-50 flex h-full w-60 flex-col items-center gap-1 overflow-y-auto p-2 transition',
+            { 'translate-x-60': sidebar, 'translate-x-0': !sidebar },
+            className
+          )}
+        >
+          <MonthCell size="xs" sidebar monthDate={currentDate} />
+          <div className="relative flex h-fit w-full flex-col">
+            {calendars?.map((calendar, index) => {
+              return (
+                <div
+                  className={clsx(
+                    'group relative flex h-8 w-full cursor-pointer flex-row items-center justify-between rounded px-2 py-1 transition duration-300',
+                    {
+                      'bg-blue-300/50 text-blue-700':
+                        JSON.stringify(selectedCalendar) ===
+                        JSON.stringify(calendar),
+                      'hover:bg-gray-100':
+                        JSON.stringify(selectedCalendar) !==
+                        JSON.stringify(calendar),
+                    }
+                  )}
+                  key={index}
+                  onClick={(e) => {
+                    if (e.target === e.currentTarget)
+                      setSelectedCalendar(calendar)
+                  }}
+                >
+                  <p>{calendar.calendarName}</p>
+                  <button
+                    onClick={() => {
+                      deleteCalendar.mutate({
+                        id: calendar.id,
+                      })
+                    }}
+                    className={clsx('invisible transition ', {
+                      'hover:text-red-500 group-hover:visible':
+                        selectedCalendar !== calendar &&
+                        calendar.calendarName !== sessionData?.user.name,
+                    })}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-5 w-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              )
+            })}
+          </div>
+
+          <div className="relative flex h-8 w-full flex-col">
+            <div
+              onClick={() => setIsCreatingCalendar(true)}
+              className="relative flex h-8 w-full cursor-pointer flex-col items-center justify-center rounded border-2 border-dashed border-gray-600 px-2 py-1 text-gray-700 transition hover:bg-gray-50"
+            >
+              <div className="text-gray relative mb-1 text-lg">+</div>
+            </div>
+            {isCreatingCalendar && (
               <div
                 className={clsx(
-                  'group relative flex h-8 w-full cursor-pointer flex-row items-center justify-between rounded px-2 py-1 transition duration-300',
+                  'absolute flex h-full w-full flex-row items-center justify-center transition',
                   {
-                    'bg-blue-300/50 text-blue-700':
-                      JSON.stringify(selectedCalendar) ===
-                      JSON.stringify(calendar),
-                    'hover:bg-gray-100':
-                      JSON.stringify(selectedCalendar) !==
-                      JSON.stringify(calendar),
+                    ' translate-x-full': !sidebar,
                   }
                 )}
-                key={index}
-                onClick={(e) => {
-                  if (e.target === e.currentTarget)
-                    setSelectedCalendar(calendar)
-                }}
               >
-                <p>{calendar.calendarName}</p>
-                <button
-                  onClick={() => {
-                    deleteCalendar.mutate({
-                      id: calendar.id,
-                    })
+                <input
+                  autoFocus
+                  id="newCalendarInput"
+                  className="absolute inset-0 h-full w-full rounded px-1 py-2 outline-2 outline-blue-300 focus:outline-blue-300"
+                  type="text"
+                  placeholder="New calendar"
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      createCalendar.mutate({
+                        name: e.currentTarget.value,
+                      })
+                      e.currentTarget.value = ''
+                      setIsCreatingCalendar(false)
+                    }
                   }}
-                  className={clsx('invisible transition ', {
-                    'hover:text-red-500 group-hover:visible':
-                      selectedCalendar !== calendar &&
-                      calendar.calendarName !== sessionData?.user.name,
-                  })}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-5 w-5"
+                />
+                <div className="absolute left-0 top-full z-10 mt-2 flex w-full flex-row items-center gap-2">
+                  <button
+                    onClick={() => {
+                      const input = document.getElementById(
+                        'newCalendarInput'
+                      ) as HTMLInputElement
+                      createCalendar.mutate({
+                        name: input.value,
+                      })
+                      input.value = ''
+                      setIsCreatingCalendar(false)
+                    }}
+                    className="relative rounded bg-blue-400 px-3 py-1 text-sm font-semibold text-gray-800 transition active:bg-blue-500"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </button>
+                    Create
+                  </button>
+                  <button
+                    onClick={() => {
+                      const input = document.getElementById(
+                        'newCalendarInput'
+                      ) as HTMLInputElement
+                      input.value = ''
+                      setIsCreatingCalendar(false)
+                    }}
+                    className="rounded bg-red-400 px-3 py-1 text-sm font-semibold text-gray-800 transition active:bg-red-500"
+                  >
+                    Cancel
+                  </button>
+                </div>
               </div>
-            )
-          })}
-        </div>
-
-        <div className="relative flex h-8 w-full flex-col">
-          <div
-            onClick={() => setIsCreatingCalendar(true)}
-            className="relative flex h-8 w-full cursor-pointer flex-col items-center justify-center rounded border-2 border-dashed border-gray-600 px-2 py-1 text-gray-700 transition hover:bg-gray-50"
-          >
-            <div className="text-gray relative mb-1 text-lg">+</div>
+            )}
           </div>
-          {isCreatingCalendar && (
-            <div
-              className={clsx(
-                'absolute flex h-full w-full flex-row items-center justify-center transition',
-                {
-                  ' translate-x-full': !sidebar,
-                }
-              )}
-            >
-              <input
-                autoFocus
-                id="newCalendarInput"
-                className="absolute inset-0 h-full w-full rounded px-1 py-2 outline-2 outline-blue-300 focus:outline-blue-300"
-                type="text"
-                placeholder="New calendar"
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    createCalendar.mutate({
-                      name: e.currentTarget.value,
-                    })
-                    e.currentTarget.value = ''
-                    setIsCreatingCalendar(false)
-                  }
-                }}
-              />
-              <div className="absolute left-0 top-full z-10 mt-2 flex w-full flex-row items-center gap-2">
-                <button
-                  onClick={() => {
-                    const input = document.getElementById(
-                      'newCalendarInput'
-                    ) as HTMLInputElement
-                    createCalendar.mutate({
-                      name: input.value,
-                    })
-                    input.value = ''
-                    setIsCreatingCalendar(false)
-                  }}
-                  className="relative rounded bg-blue-400 px-3 py-1 text-sm font-semibold text-gray-800 transition active:bg-blue-500"
-                >
-                  Create
-                </button>
-                <button
-                  onClick={() => {
-                    const input = document.getElementById(
-                      'newCalendarInput'
-                    ) as HTMLInputElement
-                    input.value = ''
-                    setIsCreatingCalendar(false)
-                  }}
-                  className="rounded bg-red-400 px-3 py-1 text-sm font-semibold text-gray-800 transition active:bg-red-500"
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          )}
         </div>
-      </div>
+      )}
     </div>
   )
 }
