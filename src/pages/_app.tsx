be0b853865896075a31ecf94ad8f5fe75e15bbe4
Pan-Grayza <zagraystepan@gmail.com>
2023-09-08 16:20:31 +0100

    Change TextColor on theme change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,25 @@ import { type AppType } from 'next/app'
 import { api } from '~/utils/api'
 import '~/styles/globals.css'
 import NavBar from '~/components/Navbar/NavBar'
+import clsx from 'clsx'
+import useStore from '~/store/useStore'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [isDarkTheme] = useStore((state) => [state.isDarkTheme])
+
   return (
-    <div className="relative flex h-screen w-screen flex-col overflow-hidden text-darkText">
+    <div
+      className={clsx(
+        'relative flex h-screen w-screen flex-col overflow-hidden text-darkText',
+        {
+          'text-darkText': isDarkTheme,
+          'text-lightText': !isDarkTheme,
+        }
+      )}
+    >
       <SessionProvider session={session}>
         <NavBar />
 
