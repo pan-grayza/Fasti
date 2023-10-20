68418bc1cd8a53fc3f2b79f4591dc4cc6adca70e
Pan-Grayza <zagraystepan@gmail.com>
2023-10-20 09:20:03 +0100

    Implemented day calendar horizontal scroll

--- src/components/Calendar/dayCalendar/DayCalendar.tsx
+++ src/components/Calendar/dayCalendar/DayCalendar.tsx
@@ -1,19 +1,63 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Schedule from './Schedule'
 import useStore from '~/store/useStore'
 import DayCell from '../CalendarComponents/DayCell'
+import { add, sub } from 'date-fns'
 
 const DayCalendar = () => {
-  const [currentDate] = useStore((state) => [
+  const [currentDate, setCurrentDate] = useStore((state) => [
     state.currentDate,
-    state.setRenamingEventNow,
+    state.setCurrentDate,
   ])
+
+  // Scroll handling
+
+  const onScrollEnd = () => {
+    const monthContainer = document.getElementById('calendarContainer')
+    const scrollX = monthContainer?.scrollLeft
+
+    if ((scrollX ?? window.innerWidth) < window.innerWidth) {
+      setCurrentDate(sub(currentDate, { days: 1 }))
+    } else if ((scrollX ?? window.innerWidth) >= window.innerWidth * 2) {
+      setCurrentDate(add(currentDate, { days: 1 }))
+    }
+    monthContainer?.scrollTo(window.innerWidth, 0)
+  }
+
+  useEffect(() => {
+    document
+      .getElementById('calendarContainer')
+      ?.addEventListener('scrollend', onScrollEnd)
+    return () => {
+      document
+        .getElementById('calendarContainer')
+        ?.removeEventListener('scrollend', onScrollEnd)
+    }
+  })
   return (
-    <div className="relative flex h-full w-full flex-col">
-      <div className="relative flex w-fit flex-col items-center justify-center px-16 py-4">
-        <DayCell date={currentDate} size="lg" dayAbr />
+    <div className="relative flex h-full w-full items-center justify-center transition-colors">
+      <div
+        id="calendarContainer"
+        className="relative flex h-full w-[300vw] snap-x snap-mandatory flex-row overflow-x-auto overflow-y-hidden scrollbar-hide"
+      >
+        {Array.from({ length: 3 }).map((week, index) => {
+          return (
+            <div
+              key={index}
+              className="relative flex h-full w-screen shrink-0 snap-center flex-col"
+            >
+              <div className="relative flex w-fit flex-col items-center justify-center px-16 py-4">
+                <DayCell
+                  date={add(currentDate, { days: index - 1 })}
+                  size="lg"
+                  dayAbr
+                />
+              </div>
+              <Schedule date={add(currentDate, { days: index - 1 })} />
+            </div>
+          )
+        })}
       </div>
-      <Schedule date={currentDate} />
     </div>
   )
 }
